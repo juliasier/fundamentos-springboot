addcb3afd2fa6ecb8525ab44ff7e7056ab2db8e0
Júlia Reis <1706juliareis@gmail.com>
2024-03-14 16:56:14 -0300

    segundo commit

--- src/main/java/br/com/juliareis/gestao_vagas/modules/candidate/conrollers/CandidateController.java
+++ src/main/java/br/com/juliareis/gestao_vagas/modules/candidate/conrollers/CandidateController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateController {
     @PostMapping("/")
     public void create(@RequestBody CandidateEntity candidateEntity) {
-
+    System.out.println("candidate");
     }
 }
